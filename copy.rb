0344e06acb7b3754eef11510084c71ef1b4d0727
Makoto Inoue <2630+makoto@users.noreply.github.com>
2020-11-09 19:38:13 +0000

    Initial commit

---
+++ copy.rb
@@ -0,0 +1,19 @@
+require 'fileutils'
+
+%w(
+  dnsprovejs
+  dnsregistrar
+  dnssec-oracle
+  ens
+  ethregistrar
+  resolver
+  subdomain-registrar
+).each do |path|
+   name = path.split('/')[-1]
+   path = "node_modules/@ensdomains/#{name}"
+   newpath = "abis/#{name}"
+   unless Dir.exists?(newpath)
+    FileUtils.mkdir_p newpath
+   end
+   system "cp -rf #{path}/build/contracts/*  #{newpath}/"
+end
